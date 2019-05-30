7c7f3e5b9bbb6f6f1a233cacbcaaeca043deedfc
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-30 22:14:14 +0000

    Mumuki Export on 2019-05-30 22:14:14 +0000

--- 00004_Agora é por $_POST/test.php
+++ 00004_Agora é por $_POST/test.php
@@ -6,18 +6,18 @@ public function testThomaz(): void {
 
     $resultado = $this->bemVindo();
     $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
-    $this->assertTrue(strtolower($resultado) == "bem-vindo thomazstaziak. foi registrado com o e-mail thomaz@digitalhouse.com", "O resultado está incorreto. Era esperado 'bem-vindo thomaz. foi registrado com o e-mail thomaz@digitalhouse.com' e foi obtido '$resultado'");
+    $this->assertTrue(strtolower($resultado) == "bem-vindo thomazstaziak. foi registrado com o e-mail thomaz@digitalhouse.com", "O resultado está incorreto. Era esperado 'Bem-vindo thomazstaziak. Foi registrado com o e-mail thomaz@digitalhouse.com' e foi obtido '$resultado'");
   }
 
   public function testVictor(): void {
-  $_POST = [
-    "email" => "victor@digitalhouse.com",
-    "usuario" => "victortorres"
-  ];
+    $_POST = [
+      "email" => "victor@digitalhouse.com",
+      "usuario" => "victortorres"
+    ];
 
-  $resultado = $this->bemVindo();
-  $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
-  $this->assertTrue(strtolower($resultado) == "bem-vindo victortorres. foi registrado com o e-mail victor@digitalhouse.com", "O resultado está incorreto. Era esperado 'bem-vindo victortorres. foi registrado com o e-mail victor@digitalhouse.com' e foi obtido '$resultado'");
+    $resultado = $this->bemVindo();
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
+    $this->assertTrue(strtolower($resultado) == "bem-vindo victortorres. foi registrado com o e-mail victor@digitalhouse.com", "O resultado está incorreto. Era esperado 'Bem-vindo victortorres. Foi registrado com o e-mail victor@digitalhouse.com' e foi obtido '$resultado'");
   }
 
   public /*...content...*/

ef3fd27280456313b20f916032e5faee327ceebe
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-30 23:26:00 +0000

    Mumuki Export on 2019-05-30 23:26:00 +0000

--- 00005_Campos opcionais/test.php
+++ 00005_Campos opcionais/test.php
@@ -1,13 +1,13 @@
 public function testThomaz(): void {
-    $_POST = [
-      "email" => "thomaz@digitalhouse.com",
-      "usuario" => "thomazstaziak"
-    ];
+  $_POST = [
+    "email" => "thomaz@digitalhouse.com",
+    "usuario" => "thomazstaziak"
+  ];
 
-    $resultado = $this->bemVindo();
-    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
-    $this->assertTrue(strtolower($resultado) == "bem-vindo thomazstaziak. foi registrado com o email thomaz@digitalhouse.com", "O resultado está incorreto. Era esperado 'bem-vindo thomaz. foi registrado com o email thomaz@digitalhouse.com' e foi obtido '$resultado'");
-  }
+  $resultado = $this->bemVindo();
+  $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
+  $this->assertTrue(strtolower($resultado) == "bem-vindo thomazstaziak. foi registrado com o e-mail thomaz@digitalhouse.com", "O resultado está incorreto. Era esperado 'Bem-vindo thomazstaziak. Foi registrado com o e-mail thomaz@digitalhouse.com' e foi obtido '$resultado'");
+}
 
   public function testVictor(): void {
     $_POST = [

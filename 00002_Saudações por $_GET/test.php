f4d517d9dcbe6735afaf2046c7a4e87a03f62dcf
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-30 19:09:38 +0000

    Mumuki Export on 2019-05-30 19:09:38 +0000

--- 00002_Saudações por $_GET/test.php
+++ 00002_Saudações por $_GET/test.php
@@ -17,7 +17,7 @@
 
     $resultado = $this->saudacoes();
     $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
-    $this->assertTrue(strtolower($resultado) == "oi walter white", "Esse resultado não é o esperado quando a URL é: http://digitalcommerce.com/saudacao.php?nome=Jesse&sobrenome=Pinkman. Era esperado 'Oi Jesse Pinkman' e obtive '$resultado'");
+    $this->assertTrue(strtolower($resultado) == "oi jesse pinkman", "Esse resultado não é o esperado quando a URL é: http://digitalcommerce.com/saudacao.php?nome=Jesse&sobrenome=Pinkman. Era esperado 'Oi Jesse Pinkman' e obtive '$resultado'");
   }
 
   public /*...content...*/

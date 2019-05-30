b5b14822aa9133481ccbae270e3109a2c318be92
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-30 22:14:17 +0000

    Mumuki Export on 2019-05-30 22:14:17 +0000

--- 00004_Agora é por $_POST/test.php
+++ 00004_Agora é por $_POST/test.php
@@ -20,4 +20,26 @@ public function testThomaz(): void {
     $this->assertTrue(strtolower($resultado) == "bem-vindo victortorres. foi registrado com o e-mail victor@digitalhouse.com", "O resultado está incorreto. Era esperado 'Bem-vindo victortorres. Foi registrado com o e-mail victor@digitalhouse.com' e foi obtido '$resultado'");
   }
 
+  public /*...content...*/public function testThomaz(): void {
+    $_POST = [
+      "email" => "thomaz@digitalhouse.com",
+      "usuario" => "thomazstaziak"
+    ];
+
+    $resultado = $this->bemVindo();
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
+    $this->assertTrue(strtolower($resultado) == "bem-vindo thomazstaziak. foi registrado com o e-mail thomaz@digitalhouse.com", "O resultado está incorreto. Era esperado 'Bem-vindo thomazstaziak. Foi registrado com o e-mail thomaz@digitalhouse.com' e foi obtido '$resultado'");
+  }
+
+  public function testVictor(): void {
+    $_POST = [
+      "email" => "victor@digitalhouse.com",
+      "usuario" => "victortorres"
+    ];
+
+    $resultado = $this->bemVindo();
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
+    $this->assertTrue(strtolower($resultado) == "bem-vindo victortorres. foi registrado com o e-mail victor@digitalhouse.com", "O resultado está incorreto. Era esperado 'Bem-vindo victortorres. Foi registrado com o e-mail victor@digitalhouse.com' e foi obtido '$resultado'");
+  }
+
   public /*...content...*/

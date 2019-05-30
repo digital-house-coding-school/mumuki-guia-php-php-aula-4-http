31fec44ec35db097315ff78af6d5fcd2d2336190
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-05-30 19:08:11 +0000

    Mumuki Export on 2019-05-30 19:08:11 +0000

--- 00002_Saudações por $_GET/description.md
+++ 00002_Saudações por $_GET/description.md
@@ -1,3 +1,3 @@
 A URL a seguir é `http://digitalecommerce.com/saudacao.php?nome=Walter&sobrenome=White`.
 
-Seu objetivo é pegar os valores de `$ _GET`, e retornar uma string que diz "Olá Walter White" (lembre-se que o nome e o sobrenome mudam de acordo com o parâmetro) não se esqueça dos espaços nas strings.
+Seu objetivo é pegar os valores de `$ _GET`, e retornar uma string que diz "Oi Walter White" (lembre-se que o nome e o sobrenome mudam de acordo com o parâmetro) não se esqueça dos espaços nas strings.
--- 00002_Saudações por $_GET/test.php
+++ 00002_Saudações por $_GET/test.php
@@ -5,8 +5,8 @@
     ];
 
     $resultado = $this->saudacoes();
-    $this->assertTrue(is_string($resultado), "Sua função não está devolvendo uma string...");
-    $this->assertTrue(strtolower($resultado) == "oi walter white", "Esse resultado não é o esperado quando a URL é: http://digitalcommerce.com/saudacao.php?nome=Walter&sobrenome=White. Era esperado 'Produto 32' e obtive '$resultado'");
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
+    $this->assertTrue(strtolower($resultado) == "oi walter white", "Esse resultado não é o esperado quando a URL é: http://digitalcommerce.com/saudacao.php?nome=Walter&sobrenome=White. Era esperado 'Oi Walter White' e obtive '$resultado'");
   }
 
   public function testJessePinkman(): void {
@@ -16,8 +16,8 @@
     ];
 
     $resultado = $this->saudacoes();
-    $this->assertTrue(is_string($resultado), "Sua função não está devolvendo uma string...");
-    $this->assertTrue(strtolower($resultado) == "oi walter white", "Esse resultado não é o esperado quando a URL é: http://digitalcommerce.com/saudacao.php?nome=Walter&sobrenome=White. Era esperado 'Produto 32' e obtive '$resultado'");
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string...");
+    $this->assertTrue(strtolower($resultado) == "oi walter white", "Esse resultado não é o esperado quando a URL é: http://digitalcommerce.com/saudacao.php?nome=Jesse&sobrenome=Pinkman. Era esperado 'Oi Jesse Pinkman' e obtive '$resultado'");
   }
 
   public /*...content...*/

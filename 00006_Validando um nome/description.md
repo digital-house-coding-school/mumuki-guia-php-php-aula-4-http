ceaac86090d7384c584a630cc337bfac583a79b6
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-06-06 16:59:58 +0000

    Mumuki Export on 2019-06-06 16:59:58 +0000

---
+++ 00006_Validando um nome/description.md
@@ -0,0 +1,9 @@
+O formulário que iremos receber via POST tem esse input:
+
+```html
+<input type="text" name="nome">
+```
+
+Sua tarefa é completar a função `validaNome` que retorna false se o nome (parâmetro) estiver vazio ou retorna true se o nome não estiver vazio.
+
+Dica: Você pode utilizar a função: [strlen](http://php.net/manual/en/function.strlen.php)
---
+++ 00006_Validando um nome/test.php
@@ -0,0 +1,23 @@
+public function testNomeCompleto(): void {
+    $_POST = [
+      "nome" => "Thomaz",
+    ];
+
+    $resultado = $this->validarNome();
+    $this->assertTrue(is_bool($resultado), "Sua função não está retornando um booleano :(");
+
+    $this->assertTrue($resultado, 'Sua função está retornando false, mas deveria retornar true');
+  }
+
+  public function testNomeVazio(): void {
+    $_POST = [
+      "nome" => "",
+    ];
+
+    $resultado = $this->validarNome();
+    $this->assertTrue(is_bool($resultado), "Sua função não está retornando um booleano :(");
+
+    $this->assertFalse($resultado, 'Sua função está retornando true, mas deveria retornar false');
+  }
+
+  public /*...content...*/

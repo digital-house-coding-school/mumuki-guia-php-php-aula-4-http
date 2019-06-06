4fa028cf9996c98d82d96ecf159760253152e94f
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-06-06 20:57:06 +0000

    Mumuki Export on 2019-06-06 20:57:06 +0000

--- 00010_Validando Senhas/description.md
+++ 00010_Validando Senhas/description.md
@@ -1,1 +1,14 @@
-teste
+O formulário que iremos receber via POST tem esses inputs:
+
+```html
+  <input type="password" name="senha">
+  <input type="password" name="confirmaSenha">
+```
+
+Sua tarefa é completar a função `validaSenha` que deve se comportar da seguinte maneira:
+
+1. Se os dois campos estiverem vazios, você deve retornar o texto "Os dois campos de senha estão vazios"
+2. Se apenas o campo "senha" estiver faltando, retorne o texto "A senha está vazia"
+3. Se somente o campo 'confirmaSenha' estiver faltando, retorne o texto "O campo confirmar senha está vazio"
+4. Se os dois campos estiverem completos, mas tiverem valores diferentes, retorne o texto "Senhas não coincidem"
+5. Se tudo estiver correto, retorne o texto "Correto"
--- 00010_Validando Senhas/test.php
+++ 00010_Validando Senhas/test.php
@@ -1,3 +1,66 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
-}
+public function testSenhaVazias(): void {
+    $_POST = [
+      "senha" => "",
+      "confirmaSenha" => "",
+    ];
+
+    $resultado = $this->validaSenha();
+
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string :(");
+
+    $this->assertTrue(strtolower($resultado) == "os dois campos de senha estão vazios", "A função está falhando com os campos vazios. Foi recebido '$resultado' e era esperado 'os dois campos de senha estão vazios'");
+  }
+
+  public function testSemSenha(): void {
+    $_POST = [
+      "senha" => "",
+      "confirmaSenha" => "123",
+    ];
+
+    $resultado = $this->validaSenha();
+
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string :(");
+
+    $this->assertTrue(strtolower($resultado) == "a senha está vazia", "A função está falhando com o campo senha vazio. Foi recebido '$resultado' e era esperado 'A senha está vazia'");
+  }
+
+  public function testSinConfirmar(): void {
+    $_POST = [
+      "senha" => "123",
+      "confirmaSenha" => "",
+    ];
+
+    $resultado = $this->validaSenha();
+
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string :(");
+
+    $this->assertTrue(strtolower($resultado) == "o campo confirmar senha está vazia", "A função está falhando com o campo confirmar senha vazio. Foi recebido '$resultado' e era esperado 'O campo confirmar senha está vazia'");
+  }
+
+  public function testSenhasDivergentes(): void {
+    $_POST = [
+      "senha" => "123",
+      "confirmaSenha" => "111",
+    ];
+
+    $resultado = $this->validaSenha();
+
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string :(");
+
+    $this->assertTrue(strtolower($resultado) == "senhas não coincidem", "A função está falhando quando as senhas são divergentes. Foi recebido '$resultado' e era esperado 'Senhas não coincidem'");
+  }
+
+  public function testCorreto(): void {
+    $_POST = [
+      "senha" => "123",
+      "confirmaSenha" => "123",
+    ];
+
+    $resultado = $this->validaSenha();
+
+    $this->assertTrue(is_string($resultado), "Sua função não está retornando uma string :(");
+
+    $this->assertTrue(strtolower($resultado) == "correto", "A função está falhando quando as senhas estão corretas. Foi recebido '$resultado' e era esperado 'Correto'");
+  }
+
+  public /*...content...*/
